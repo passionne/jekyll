b2d0675b95c1643bd2fda2f11bb454d8991da017
Matthieu Fernandes <matthieu.fernandes@gmail.com>
2015-04-23 09:44:06 +0200

    add script (build.sh) to generate and publish lastest modification

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+bundle exec jekyll build
+
+git checkout gh-pages
+
+cp _site/* .
+cp _site/.* .
+
+git add -A
+git commit -am "auto-commit generated pages at $(date "+%Y-%m-%d_%H-%M-%S")"
+
+#git push origin gh-pages
+

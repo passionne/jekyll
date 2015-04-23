b5232cf5e0f7115584dacf5913646a6f02ab6ade
Matthieu Fernandes <matthieu.fernandes@gmail.com>
2015-04-23 10:02:00 +0200

    improve script to generate site + exclude build.sh from site

--- build.sh
+++ build.sh
@@ -5,11 +5,12 @@ bundle exec jekyll build
 
 git checkout gh-pages
 
-cp _site/* .
-cp _site/.* .
+rm -rf .sass-cache
+rsync -av _site/ .
 
 git add -A
 git commit -am "auto-commit generated pages at $(date "+%Y-%m-%d_%H-%M-%S")"
 
-#git push origin gh-pages
+git push origin gh-pages
+git checkout master
 
